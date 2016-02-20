d657f9ce0ce3132a891ba851ca2e30b7e4179381
herlanassis <herlanassis@gmail.com>
2016-02-20 00:26:48 -0300

    Corrigido big do log, falta chegar o bug dos grupos e a filtragem de usuários

--- cadastro/forms.py
+++ cadastro/forms.py
@@ -1,10 +1,33 @@
 import re
 from django import forms
+from django.contrib.auth import (authenticate, get_user_model,
+	password_validation,)
 from django.utils.encoding import smart_text
 from localflavor.br.forms import BRPhoneNumberField, BRCPFField
 from input_mask.contrib.localflavor.br.widgets import (BRPhoneNumberInput,
 	BRCPFInput)
+from django.utils.translation import ugettext, ugettext_lazy as _
 from .models import *
+
+class ReadOnlyPasswordHashWidget(forms.Widget):
+	def render(self, name, value, attrs):
+		encoded = value
+		final_attrs = self.build_attrs(attrs)
+
+		if not encoded or encoded.startswith(UNUSABLE_PASSWORD_PREFIX):
+			summary = mark_safe("<strong>%s</strong>" % ugettext("No password set."))
+		else:
+			try:
+				hasher = identify_hasher(encoded)
+			except ValueError:
+				summary = mark_safe("<strong>%s</strong>" % ugettext("Invalid password format or unknown hashing algorithm."))
+			else:
+				summary = format_html_join('',
+					"<strong>{}</strong>: {} ",
+					((ugettext(key), value)
+						for key, value in hasher.safe_summary(encoded).items()))
+
+		return format_html("<div{}>{}</div>", flatatt(final_attrs), summary)
 
 class CPFInput(BRCPFInput):
 	def render(self, name, value, attrs=None):
@@ -42,8 +65,40 @@ class FormLogradouro(forms.ModelForm):
 		fields = ['nome', 'bairro']
 
 class FormUsuario(forms.ModelForm):
+	error_messages = {
+		'password_mismatch': _("The two password fields didn't match."),
+	}
+
 	cpf = BRCPFField(widget=CPFInput, label='CPF')
 	telefone = BRPhoneNumberField(widget=BRPhoneNumberInput, required=False)
+	password1 = forms.CharField(label=_("Password"), 
+		widget=forms.PasswordInput)
+	password2 = forms.CharField(label=_("Password confirmation"),
+		widget=forms.PasswordInput,
+	help_text=_("Enter the same password as before, for verification."))
+
+	def __init__(self, *args, **kwargs):
+		super(FormUsuario, self).__init__(*args, **kwargs)
+		self.fields['cpf'].widget.attrs.update({'autofocus': ''})
+
+	def clean_password2(self):
+		password1 = self.cleaned_data.get("password1")
+		password2 = self.cleaned_data.get("password2")
+		if password1 and password2 and password1 != password2:
+			raise forms.ValidationError(
+				self.error_messages['password_mismatch'],
+				code='password_mismatch',
+			)
+		self.instance.cpf = self.cleaned_data.get('cpf')
+		password_validation.validate_password(self.cleaned_data.get('password2'), self.instance)
+		return password2
+
+	def save(self, commit=True):
+		user = super(FormUsuario, self).save(commit=False)
+		user.set_password(self.cleaned_data["password1"])
+		if commit:
+			user.save()
+		return user
 
 	def clean_telefone(self):
 		value = self.cleaned_data['telefone']
@@ -57,7 +112,7 @@ class FormUsuario(forms.ModelForm):
 		model = Usuario
 
 		fields = ['cpf', 'tipo', 'nome', 'sobrenome', 'email',
-		'telefone', 'nascimento', 'endereco', 'codigo', 'password']
+		'telefone', 'nascimento', 'endereco', 'codigo',]
 
 class FormCliente(forms.ModelForm):
 	cpf = BRCPFField(widget=CPFInput, label='CPF')
--- cadastro/management/commands/associar_usuarios_a_grupos.py
+++ cadastro/management/commands/associar_usuarios_a_grupos.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.core.management import BaseCommand
-from siade.agentes.models import Agente
+from cadastro.models import Usuario
 from django.contrib.auth.models import Group
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        for num, nome in Agente.Tipo.choices:
+        for num, nome in Usuario.Tipo.choices:
             grupo, c = Group.objects.get_or_create(name=nome)
-            agentes_do_tipo = Agente.objects.filter(tipo=num)
-            grupo.user_set.add(*agentes_do_tipo)
+            usuarios_do_tipo = Usuario.objects.filter(tipo=num)
+            grupo.user_set.add(*usuarios_do_tipo)
