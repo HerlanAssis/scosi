89afbdd79544868a8f2c58973b9cce9c53c3c5ee
herlanassis <herlanassis@gmail.com>
2016-02-24 13:37:07 -0300

    Corrigindo settings.py

--- scosi/settings.py
+++ scosi/settings.py
@@ -130,11 +130,9 @@ USE_TZ = True
 
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
